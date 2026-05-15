ba66920122294413a23896f9c18a54481de5e4e6
veera-vijay <veera53163@gmail.com>
2026-05-15 18:43:51 +0530

    last commit

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -9,7 +9,7 @@ const generateToken = (user) => {
     id: user._id,
     username: user.username,
     email: user.email,
-    role: user.role,
+     role: user.role 
   };
 
   const token = jwt.sign(
